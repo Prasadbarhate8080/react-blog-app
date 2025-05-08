7f080b9c826a12273f0503efcb38bb1846f0a980
prasadbarhate8080 <prasadbarhate388@gmail.com>
2025-05-08 07:50:47 +0530

    fixed bug

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ import Container from "./Container/Container.jsx"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-import Signup from "./Signup";
+import SignUp from "./Signup.jsx";
 import Login from "./Login.jsx";
 import Button from "./Button";
 import PostForm from "./post-form/PostForm";
@@ -19,7 +19,7 @@ export {
     Logo,
     LogoutBtn,
     RTE,
-    Signup,
+    SignUp,
     Login,
     Button,
     PostForm,
